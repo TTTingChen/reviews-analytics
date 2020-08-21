34f34a08de86c8da6b587c15f0f24d634299cdca
Turtle <user@userde-MacBook-Air.local>
2020-08-22 04:58:05 +0800

    calc avg len of reviews

--- read.py
+++ read.py
@@ -6,8 +6,15 @@ with open('reviews.txt', 'r') as f:
 		count = count + 1
 		if count % 1000 == 0:#每讀1000筆印出來
 			print(len(data))
-print(len(data))
+print('檔案讀取完了, 總共有', len(data), '筆資料')
+
+sum_len = 0
+for  d in data:
+	sum_len = sum_len + len(d)
+	
+print('留言的平均長度為', sum_len/len(data))
+
 #print(data)
-print(data[0])
-print('---------')
-print(data[1])
+#print(data[0])
+#print('---------')
+#print(data[1])
